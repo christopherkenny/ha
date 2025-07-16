0aae37c12271bc7b654ed46e20eb3d5438f58f3c
Christopher Kenny <ctk44@cornell.edu>
2025-07-15 22:28:45 -0400

    start prep for general use

---
+++ R/api.R
@@ -0,0 +1,14 @@
+ha_api <- function(ip = NULL) {
+
+  base_url <- api_url(local = is.null(ip), ip = ip)
+
+  req <- httr2::request(base_url = base_url) |>
+    httr2::req_url_path_append('/') |>
+    httr2::req_auth_bearer_token(token = ha_get_key()) |>
+    httr2::req_headers(
+      `Content-Type` = 'application/json'
+    )
+
+  req |>
+    httr2::req_perform()
+}
--- R/api_url.R
+++ R/api_url.R
@@ -1,3 +1,11 @@
-local_url <- function() {
-  'http://localhost:8123/api'
+api_url <- function(local = TRUE, ip = NULL) {
+  if (local) {
+    'http://localhost:8123/api'
+  } else {
+    if (is.null(ip)) {
+      cli::cli_abort(c('No IP address provided.',
+                       i = 'Set {.code ip} to the Home Assistant instance IP address.'))
+    }
+    paste0('http://', ip, ':8123/api')
+  }
 }
