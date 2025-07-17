70cee28c1b196f690b2873d2124c4d0aa790bc43
Christopher Kenny <ctk44@cornell.edu>
2025-07-17 07:47:55 -0400

    error log

---
+++ R/api_error_log.R
@@ -0,0 +1,23 @@
+#' Retrieve Home Assistant Error Log
+#'
+#' @param ip `r template_var_ip()`
+#'
+#' @returns A list containing the Home Assistant error log
+#' @export
+#'
+#' @examplesIf ha_has_key()
+#' ha_api_error_log()
+ha_api_error_log <- function(ip = NULL) {
+  base_url <- api_url(local = is.null(ip), ip = ip)
+
+  req <- httr2::request(base_url = base_url) |>
+    httr2::req_url_path_append('error_log') |>
+    httr2::req_auth_bearer_token(token = ha_get_key()) |>
+    httr2::req_headers(
+      `Content-Type` = 'application/json'
+    )
+
+  req |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
