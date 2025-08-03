e2e123d80a5d42d35f86d1fbcd110eef65aebcb0
Christopher Kenny <ctk44@cornell.edu>
2025-08-03 15:56:40 -0400

    calendar

--- R/api_calendar.R
+++ R/api_calendar.R
@@ -1,17 +1,17 @@
-#' Retrieve Home Assistant Events
+#' Retrieve Home Assistant calendar
 #'
 #' @param ip `r template_var_ip()`
 #'
-#' @returns A list containing the Home Assistant events.
+#' @returns A list containing the Home Assistant calendar.
 #' @export
 #'
 #' @examplesIf ha_has_key()
-#' ha_api_events()
-ha_api_events <- function(ip = NULL) {
+#' ha_api_calendars()
+ha_api_calendars <- function(ip = NULL) {
   base_url <- api_url(local = is.null(ip), ip = ip)
 
   req <- httr2::request(base_url = base_url) |>
-    httr2::req_url_path_append('events') |>
+    httr2::req_url_path_append('calendars') |>
     httr2::req_auth_bearer_token(token = ha_get_key()) |>
     httr2::req_headers(
       `Content-Type` = 'application/json'
